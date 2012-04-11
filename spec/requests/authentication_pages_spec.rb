f0a098a61ed44c69ea4488d5abf1d948e7956da2
Ivan Calderon <iwano@hotmail.com>
2012-04-10 21:33:23 -0500

    Exercises ch10

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -72,12 +72,25 @@ describe "Authentication" do
             fill_in "Password", with: user.password
             click_button "Sign in"
           end
-           describe "after signing in" do
+          describe "after signing in" do
 
             it "should render the desired protected page" do
               page.should have_selector('title', text: 'Edit user')
             end
-          end
+
+            describe "when signing in again" do
+              before do
+                visit signin_path
+                fill_in "Email",    with: user.email
+                fill_in "Password", with: user.password
+                click_button "Sign in"
+              end
+
+              it "should render the default (profile) page" do
+                page.should have_selector('title', text: user.name) 
+              end
+            end
+          end 
         end      
       end
       
